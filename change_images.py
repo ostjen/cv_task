f130912e464447e72ef4360c2d1abaa1f704e500
ostjen <leonardoostjen@gmail.com>
2019-07-25 20:04:25 -0300

    rotation script added

---
+++ change_images.py
@@ -0,0 +1,35 @@
+from keras.models import load_model
+import pandas as pd
+import cv2
+import numpy as np
+import glob
+
+
+def rotate(model,image):
+    image_pred = image.reshape(1,64,64,3)
+    output = model.predict(image_pred)
+    value = output.argmax()          #get the index of the encoded output
+    angle = 0
+    
+    if value  == 1:      #right
+        angle = 90
+
+    elif value == 0:    #left
+        angle = 270
+
+    elif value == 3:    #down
+        angle = 180
+        
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+    return result
+
+
+if __name__ == '__main__':
+	model = load_model('model.h5')
+	for file in glob.glob("data/*.jpg"):
+		image_aux = cv2.imread(file,1)
+		new_image = rotate(model,image_aux)
+		cv2.imwrite(file,new_image)
+
