b4abdaf936d83d2c32e38b098e30d3389e05a4c3
ostjen <leonardoostjen@gmail.com>
2019-07-25 18:56:26 -0300

    new NN and end of overfitting (almost 95% accuracy)

--- cv_task_keras.py
+++ cv_task_keras.py
@@ -9,31 +9,19 @@ import pandas as pd
 from keras.models import Sequential
 from keras.optimizers import RMSprop
 from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers import Activation, Dropout, Flatten, Dense, GlobalAveragePooling2D, Lambda, Dropout
 import numpy as np
 from keras.utils.np_utils import to_categorical
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 import keras.backend as K
 
+target = pd.read_csv('./train.truth.csv')
+names = target['fn']
 
-
-images = [cv2.imread(file,0) for file in glob.glob("data/*.jpg")]
-
-
-plt.imshow(images[2],cmap = plt.cm.gray) 
-
-
-images_b = []
-for i in range(len(images)):
-    blur = cv2.GaussianBlur(images[i], (5, 5), 0)
-    images_b.append(blur)
-
-
-plt.imshow(images_b[2],cmap = plt.cm.gray)
-
-
-target = pd.read_csv('./data/train.truth.csv')
-
+images = []
+for name in names:
+    aux = './data/' + name
+    images.append(cv2.imread(aux,1))
 
 
 LE = LabelEncoder()
@@ -42,7 +30,7 @@ target = np.array(target)
 
 from keras.utils import to_categorical
 
-train_x,test_x,train_y,test_y = train_test_split(images_b,target,test_size = 0.3)
+train_x,test_x,train_y,test_y = train_test_split(images,target,test_size = 0.33)
 
 train_x = np.array(train_x)
 train_y = np.array(train_y)
@@ -53,10 +41,8 @@ train_y = to_categorical(train_y)
 test_y = to_categorical(test_y)
 
 
-
-
-train_x = train_x.reshape(34227,64,64,-1)
-test_x = test_x.reshape(14669,64,64,-1)
+#train_x = train_x.reshape(34227,64,64,3)
+#test_x = test_x.reshape(14669,64,64,3)
 
 #using -1 numpy infers shape
 
@@ -81,14 +67,14 @@ train_x /= 255
 test_x /= 255
 
 
-
+'''
 model = Sequential()
 model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(64,64, 1)))
+model.add(Dropout(0.50))
 model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(512, activation='relu', kernel_initializer='he_uniform'))
 model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dropout(0.50))
 model.add(Dense(4, activation='softmax'))
 
 
@@ -98,23 +84,53 @@ sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
+'''
+# Model creation
 
 
+def pure_cnn():
+    
+    model = Sequential()
+    
+    model.add(Conv2D(96, (3, 3), activation='relu', padding = 'same', input_shape=(64,64,3)))    
+    model.add(Dropout(0.2))
+    
+    model.add(Conv2D(96, (3, 3), activation='relu', padding = 'same'))  
+    model.add(Conv2D(96, (3, 3), activation='relu', padding = 'same', strides = 2))    
+    model.add(Dropout(0.5))
+    
+    model.add(Conv2D(192, (3, 3), activation='relu', padding = 'same'))    
+    model.add(Conv2D(192, (3, 3), activation='relu', padding = 'same'))
+    model.add(Conv2D(192, (3, 3), activation='relu', padding = 'same', strides = 2))    
+    model.add(Dropout(0.5))    
+    
+    model.add(Conv2D(192, (3, 3), padding = 'same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(192, (1, 1),padding='valid'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(4, (1, 1), padding='valid'))
 
-model.summary()
+    model.add(GlobalAveragePooling2D())
+    
+    model.add(Activation('softmax'))
 
+    model.summary()
+    
+    return model
 
+from keras.optimizers import Adam, SGD
+model = pure_cnn()
+model.compile(loss='categorical_crossentropy', # Better loss function for neural networks
+              optimizer=Adam(lr=0.0001), # Adam optimizer with 1.0e-4 learning rate
+              metrics = ['accuracy']) # Metrics to be evaluated by the model
 
-model.fit(train_x,train_y,
-          
-              batch_size=32,
-              epochs= 30,
-              validation_data = (test_x,test_y),
-              shuffle=True,
-              verbose=2)
+model_details = model.fit(train_x, train_y,
+                    batch_size = 32,
+                    epochs = 20, # number of iterations
+                    validation_data= (test_x,test_y),
+                    verbose=1)
 
-
-model.save('model.h5')
+model.save('model_nogaussian.h5')
 
 
 
